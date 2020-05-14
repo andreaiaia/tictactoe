e30d1a1136c12862a0a81598172c2ba1fda052aa
Andrea Bianchi <bianan96@gmail.com>
2020-05-14 15:10:02 +0200

    made and style page

---
+++ script.js
@@ -0,0 +1,14 @@
+var gameboard = (function() {
+  let _board = [['','',''], 
+                ['','',''], 
+                ['','','']];
+  
+  function _checkScore() {
+    _board.forEach(row => {
+      // if someone win call _endgame
+    });
+  }
+
+  function _endgame() {}
+
+})();
