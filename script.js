16302a2d4c8c9a398f75d718d3b4fd2b33fe3733
Andrea Bianchi <bianan96@gmail.com>
2020-05-14 15:33:03 +0200

    try to make the gameboard object

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 var gameboard = (function() {
-  let _board = [['','',''], 
-                ['','',''], 
-                ['','','']];
+  let _board = [];
   
+  // cache DOM
+  let cells = document.querySelectorAll('.cell');
+
+  // bins events
+  cells.forEach(cell => cell.addEventListener('onclick', move))
+
   function _checkScore() {
     _board.forEach(row => {
       // if someone win call _endgame
@@ -11,4 +15,14 @@ var gameboard = (function() {
 
   function _endgame() {}
 
+  function move() {
+    _board.push('hey');
+    console.log(_board);
+    
+    console.log('move');
+  }
+
+  return {
+    move: move,
+  }
 })();
