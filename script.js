5ac78ab71790ea19c851f591c3af0d02c51972f9
Andrea Bianchi <bianan96@gmail.com>
2020-05-15 14:33:45 +0200

    made event listeners in match

--- script.js
+++ script.js
@@ -10,17 +10,43 @@ const playerFactory = (player, symbol) => {
   }
 };
 
-const gameboard = function() {
+const gameboard = (function() {
   const _board = ['','','',
                   '','','',
                   '','',''];
-};
+  
+  function updateBoard(i) {
+    _board[i] = '1'; //some stuff
+    console.log(_board);
+    _checkScore();
+  }
+
+  function _checkScore() {}
+
+  return {
+    updateBoard,
+  }
+})();
 
 const match = (function() {
 
-  // chache DOM
-  const player1Name = document.querySelector('.player1Name');
-  const player2Name = document.querySelector('.player2Name');
+  // cache DOM
+  const _player1Name = document.querySelector('.player1Name');
+  const _player2Name = document.querySelector('.player2Name');
+  const _board = document.querySelectorAll('.cell');
 
-  const player1 = playerFactory(player1Name.value,'x');
+  // bind events
+  _board.forEach(cell => cell.addEventListener('click', move));
+
+  const player1 = playerFactory(_player1Name.value,'x');
+  const player2 = playerFactory(_player2Name.value,'x');
+
+  function move(e) {
+    let i = e.target.dataset.index;
+    gameboard.updateBoard(i);
+  };
+
+  return {
+    move,
+  }
 })();
