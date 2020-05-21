ebd229545f4a0db78baa61ca4703ba2034e1f573
Andrea Bianchi <bianan96@gmail.com>
2020-05-21 12:22:05 +0200

    add winning condition checker

--- script.js
+++ script.js
@@ -11,21 +11,18 @@ const playerFactory = (player, symbol) => {
 };
 
 const gameboard = (function() {
-  const _board = ['','','',
-                  '','','',
-                  '','',''];
-  const _winComb = ['012', '345', '678',
-                    '036', '147', '258',
-                    '048', '246'];
+  const _board = [0,0,0,
+                  0,0,0,
+                  0,0,0];
   
   // cache DOM
   const _gameGrid = document.querySelectorAll('.cell');
 
   function _render() {
     for (let i = 0; i < _board.length; i++) {
-      if (_board[i] === 'x') {
+      if (_board[i] === 1) {
         _gameGrid[i].classList.add('mdi-close');
-      } else if (_board[i] === 'o') {
+      } else if (_board[i] === 4) {
         _gameGrid[i].classList.add('mdi-circle-outline');
       }
     }
@@ -33,30 +30,44 @@ const gameboard = (function() {
   }
   
   function updateBoard(i, player) {
-    if (player && _board[i] === '') {
-      _board[i] = 'x'; // x
-    } else if (_board[i] === '') {
-      _board[i] = 'o'; // o
+    if (player && _board[i] === 0) {
+      _board[i] = 1; // x
+    } else if (_board[i] === 0) {
+      _board[i] = 4; // o
     }
-    _checkScore();
+    _checkScore(i, player);
     _render();
   }
 
-  function _checkScore() {
-    let xScore = '';
-    let oScore = '';
-    for (let i = 0; i < _board.length; i++) {
-      if (_board[i] === 'x') xScore += i;
-      else if (_board[i] === 'o') oScore += i;
+  function _checkScore(cell, player) {
+    // check Column
+    var precCol = +cell - 3 < 0 ? _board.length + +cell - 3 : +cell - 3;
+    var succCol = +cell + 3 > 8 ? +cell + 3 - 9 : +cell + 3;
+    var colSum = _board[cell] + _board[precCol] + _board[succCol];
+    if (colSum === 3 || colSum === 12) _winning(player);
+
+    // check Row
+    let rowSum;
+    if (0 <= cell && cell < 3) {
+      rowSum = _board[0] + _board[1] + _board[2];
+    } else if (3 <= cell && cell < 6) {
+      rowSum = _board[3] + _board[4] + _board[5];
+    } else if (6 <= cell && cell < 9) {
+      rowSum = _board[6] + _board[7] + _board[8];
     }
-    console.log(xScore);
-    
-    if (_winComb.includes(xScore)) _winning('player1');
-    else if (_winComb.includes(oScore)) _winning('player2')
+    if (rowSum === 3 || rowSum === 12) _winning(player);
+
+    // check Diag
+    if ('02468'.includes(cell)) {
+      let diagA = _board[0] + _board[4] + _board[8];
+      let diagB = _board[2] + _board[4] + _board[6];
+      if (diagA === 3 || diagA === 12 || diagB === 3 || diagB === 12) _winning(player);
+    }
   }
 
   function _winning(player) {
-    console.log(`${player} won!`);
+    let name = player ? 'x' : 'o';
+    console.log(`${name} won!`);
   }
 
   return {
